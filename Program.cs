fcfd22f25625f371bcaa42753f0c6d6dbb979b51
Farhan Younas <farhanyounas5204@gmail.com>
2021-12-30 17:58:23 +0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace prog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //seperately execute each expression
+            int a,b,c;
+            a=2;
+            b=3;
+            c=5;
+        
+            Console.WriteLine(++a);  
+            
+            Console.WriteLine(a*b*c); 
+            
+            Console.WriteLine(a++);    
+            
+            Console.WriteLine(b+=a);  
+            
+            Console.WriteLine(b+=a*c);  
+            
+            //Console.WriteLine(a++b);   //invalid
+            
+            Console.WriteLine(Convert.ToChar(a+=2, 2/2, a*b)); 
+            
+            //Console.WriteLine(++b++);   //invalid
+            
+            Console.WriteLine(a++ + b++); 
+        }
+    }
+}
